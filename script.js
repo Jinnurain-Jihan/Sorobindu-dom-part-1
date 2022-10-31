dd834c20f8610b8765746127e3b5f4ebce87a861
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-10-31 20:09:30 +0600

    DOM part one

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(document.title);
+document.title="Javascript DOM";
+console.log(document.title);
+console.log(document.URL);
+console.log(document.head);
+console.log(document.body);
+console.log(document.images);
+console.log(document.links);
+console.log(document.scripts);
+console.log(document.styleSheets);
+console.log(document.forms);
